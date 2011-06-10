e3d72cb1f7711bbb8ee970f1e40f600046810f7a
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-10 23:06:25 +0200

    bootmenu busybox no more used

--- jordan.mk
+++ jordan.mk
@@ -99,7 +99,6 @@ PRODUCT_PACKAGES += \
     lights.jordan \
     libaudiopolicy \
     bootmenu \
-    bootmenu_busybox \
     Usb
 
 # Add DroidSSHd (dropbear) Management App - tpruvot/android_app_droidsshd @ github
