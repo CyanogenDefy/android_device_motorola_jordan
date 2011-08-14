48838aa4526a56c96f372b7a42e7c9b384c5c4d4
Quarx2k <agent00791@gmail.com>
2011-08-14 21:54:41 +0700

    build only for jordan
    optional > eng

--- mot_boot_mode/Android.mk
+++ mot_boot_mode/Android.mk
@@ -1,4 +1,5 @@
 # Copyright 2005 The Android Open Source Project
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),jordan)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -6,11 +7,11 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
         mot_boot_mode.c
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := mot_boot_mode
 
 LOCAL_SHARED_LIBRARIES := libcutils libc
 
 include $(BUILD_EXECUTABLE)
 
-
+endif 
