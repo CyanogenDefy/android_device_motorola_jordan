aa318eb75a64c9c4cadb57baedaac21e05a12a1c
Danny Baumann <dannybaumann@web.de>
2011-08-26 08:40:58 +0200

    Remove some unused and/or duplicated properties.
    Change-Id: Ieecbc634d785c58ab8992c94c18f0c5f9e820791

--- jordan.mk
+++ jordan.mk
@@ -34,28 +34,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.media.capture.torchIntensity=25 \
 	ro.media.capture.classification=classH \
 	ro.media.capture.flip=horizontalandvertical \
-	ro.com.android.wifi-watchlist=GoogleGuest \
-	ro.error.receiver.system.apps=com.google.android.feedback \
-	ro.setupwizard.enterprise_mode=1 \
-	ro.com.google.clientidbase=android-verizon \
 	ro.com.google.locationfeatures=1 \
-	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-	ro.config.vc_call_vol_steps=7 \
 	ro.telephony.call_ring.multiple=false \
-	windowsmgr.max_events_per_sec=70 \
 	ro.telephony.call_ring.delay=3000 \
 	ro.url.safetylegal=http://www.motorola.com/staticfiles/Support/legal/?model=MB525 \
-	ro.setupwizard.enable_bypass=1 \
 	ro.media.dec.jpeg.memcap=20000000 \
 	ro.media.dec.aud.wma.enabled=1 \
 	ro.media.dec.vid.wmv.enabled=1 \
-	ro.product.multi_touch_enabled=true \
-	ro.com.motorola.smartsensor=true \
 	dalvik.vm.lockprof.threshold=500 \
 	ro.kernel.android.checkjni=0 \
-	dalvik.vm.dexopt-data-only=1 \
-	dalvik.vm.dexopt-flags=m=y
+	dalvik.vm.dexopt-data-only=1
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/jordan/overlay
 
