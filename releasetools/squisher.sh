4c725638112e8a2a03a503273c791a5dc37c079f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-09-03 15:58:23 +0200

    fix xterm terminfo folder

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -7,7 +7,6 @@ rm -f $REPACK/ota/system/app/FM.apk
 rm -f $REPACK/ota/system/xbin/irssi
 
 mkdir -p $REPACK/ota/system/etc/terminfo/x
-mkdir -p $REPACK/ota/system/etc/terminfo/x/xterm
 cp $REPACK/ota/system/etc/terminfo/l/linux $REPACK/ota/system/etc/terminfo/x/xterm
 
 # prebuilt boot, devtree, logo & updater-script
