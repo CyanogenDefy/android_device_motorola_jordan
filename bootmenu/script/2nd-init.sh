c9274d71a2487eecaa9993666f716549e421aeb2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-09-05 18:11:44 +0200

    fix missing ueventd (sbin) in standard profile

--- bootmenu/script/2nd-init.sh
+++ bootmenu/script/2nd-init.sh
@@ -16,6 +16,7 @@ export PATH=/sbin:/system/xbin:/system/bin
 mount -o remount,rw /
 rm -r -f /*.rc
 cp -r -f /system/bootmenu/2nd-init/* /
+ln -s /init /sbin/ueventd
 chmod 755 /*.rc
 chmod 4755 /system/bootmenu/binary/2nd-init
 
