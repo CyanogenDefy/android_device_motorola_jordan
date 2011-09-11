bbdb50ef1e6f7e34a26c2ddd3139b3caf0401608
Quarx2k <agent00791@gmail.com>
2011-09-11 15:38:36 +0700

    Update to 2.3.4 kernel
    update squisher for 2.3.4 kernel

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -21,9 +21,9 @@ rm -f $REPACK/ota/boot.img
 cp -f $ANDROID_BUILD_TOP/device/motorola/jordan/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
 if [ -n "$CYANOGEN_RELEASE" ]; then
   cat $ANDROID_BUILD_TOP/device/motorola/jordan/updater-script-rel >> $REPACK/ota/META-INF/com/google/android/updater-script
-  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/boot-222-179-4.smg $REPACK/ota/boot.img
-  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/devtree-222-179-2.smg $REPACK/ota/devtree.img
-  cp -f $ANDROID_BUILD_TOP/device/motorola/jordan/logo-google.raw $REPACK/ota/logo.img
+  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/boot-234-134.smg $REPACK/ota/boot.img
+  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/devtree-234-134.smg $REPACK/ota/devtree.img
+  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/logo-moto.raw $REPACK/ota/logo.img
 fi
 cp -f $ANDROID_BUILD_TOP/out/target/product/jordan/root/init $REPACK/ota/system/bootmenu/2nd-init/init
 cp -f $ANDROID_BUILD_TOP/out/target/product/jordan/root/init.rc $REPACK/ota/system/bootmenu/2nd-init/init.rc
