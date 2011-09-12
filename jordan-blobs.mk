a2eb3d7f21a07a41e25b4a8f8a355e31b335ad22
Danny Baumann <dannybaumann@web.de>
2011-09-12 14:28:09 +0200

    Remove manufacturing-only stuff.
    Change-Id: Id92feea09b1c7a88c76551592105cf56b7709984

--- jordan-blobs.mk
+++ jordan-blobs.mk
@@ -30,8 +30,6 @@ PRODUCT_COPY_FILES += \
 	device/motorola/jordan/prebuilt/etc/wifi/dnsmasq.conf:system/etc/wifi/dnsmasq.conf \
 	device/motorola/jordan/prebuilt/etc/wifi/tiwlan.ini:system/etc/wifi/tiwlan.ini \
 	device/motorola/jordan/prebuilt/etc/wifi/tiwlan_ap.ini:system/etc/wifi/tiwlan_ap.ini \
-	device/motorola/jordan/prebuilt/etc/wifi/tiwlan_ap_rfmd.ini:system/etc/wifi/tiwlan_ap_rfmd.ini \
-	device/motorola/jordan/prebuilt/etc/wifi/tiwlan_rfmd.ini:system/etc/wifi/tiwlan_rfmd.ini \
 	device/motorola/jordan/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	device/motorola/jordan/prebuilt/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
 	device/motorola/jordan/prebuilt/etc/location.cfg:system/etc/location.cfg \
