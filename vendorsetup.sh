9e30c89f4a82fff1d224fbd0c34784c6c218916f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-09-20 13:49:31 +0200

    add partitions for future bootmenu use

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2008 The Android Open Source Project
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_jordan-userdebug
+# add_lunch_combo generic_jordan-userdebug
 
