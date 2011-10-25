d72fedede287446dc1740a564f82ad78929bcb69
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-25 12:28:24 +0200

    use our own logo and restore irssi like official

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -3,7 +3,7 @@
 
 # Delete unwanted apps
 rm -f $REPACK/ota/system/app/RomManager.apk
-rm -f $REPACK/ota/system/xbin/irssi
+# rm -f $REPACK/ota/system/xbin/irssi
 
 mkdir -p $REPACK/ota/system/etc/terminfo/x
 cp $REPACK/ota/system/etc/terminfo/l/linux $REPACK/ota/system/etc/terminfo/x/xterm
@@ -15,7 +15,7 @@ if [ -n "$CYANOGEN_RELEASE" ]; then
   cat $ANDROID_BUILD_TOP/device/motorola/jordan/updater-script-rel >> $REPACK/ota/META-INF/com/google/android/updater-script
   cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/boot-222-179-4.smg $REPACK/ota/boot.img
   cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/devtree-222-179-2.smg $REPACK/ota/devtree.img
-  cp -f $ANDROID_BUILD_TOP/vendor/motorola/jordan/logo-moto.raw $REPACK/ota/logo.img
+  cp -f $ANDROID_BUILD_TOP/device/motorola/jordan/logo-google.raw $REPACK/ota/logo.img
 fi
 cp -f $ANDROID_BUILD_TOP/out/target/product/jordan/root/init $REPACK/ota/system/bootmenu/2nd-init/init
 cp -f $ANDROID_BUILD_TOP/out/target/product/jordan/root/init.rc $REPACK/ota/system/bootmenu/2nd-init/init.rc
