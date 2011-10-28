02b22b9f4ac07fa1c52a91476246ac052d41d154
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-28 23:17:42 +0200

    Various small config fixes for now and the future...
    - chown /data/battd for the new battd binary
    - enhance wide video bitrate to 3.5 Mbps, and maximum value to 4 Mbps
    - bootmenu adbd : load ondemand governor and vars for /tmp history
    - variables/cflags for gerrit patches
    - update busybox fstab for pds
    - fix TW defy parts translation
    - vendor blob extraction scripts (adb pull)
      Not required, we have a vendor repository, but can be usefull to
      refresh or compare files from a new SBF version, like Defy+ soon
    
    Change-Id: I51c1c676970e84075ab806c4f3aa3778a664c21a

--- bootmenu/script/adbd.sh
+++ bootmenu/script/adbd.sh
@@ -18,6 +18,10 @@ echo charge_adb > /dev/usb_device_mode
 # load overclock settings to reduce heat and battery use
 /system/bootmenu/script/overclock.sh
 
+# busybox ash history
+export HISTFILE=/tmp/.ash_history
+export HISTFILESIZE=256
+
 PATH=/sbin:/system/xbin:/system/bin /sbin/adbd.root &
 
 exit
