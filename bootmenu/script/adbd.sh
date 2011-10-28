100bdd81b1857195e4e4e1e0f7e7beb464f9fd47
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-29 00:51:58 +0200

    bootmenu: load overclock modules before adbd

--- bootmenu/script/adbd.sh
+++ bootmenu/script/adbd.sh
@@ -15,6 +15,9 @@ sleep 1
 
 echo charge_adb > /dev/usb_device_mode
 
+# load overclock settings to reduce heat and battery use
+/system/bootmenu/script/overclock.sh
+
 PATH=/sbin:/system/xbin:/system/bin /sbin/adbd.root &
 
 exit
