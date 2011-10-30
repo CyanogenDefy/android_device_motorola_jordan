6d8d02954c21a296f6ebf23b33a2c119c2c06493
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-30 14:12:46 +0100

    do not prelink camera wrapper

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -9,5 +9,7 @@ LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
 LOCAL_MODULE := libcamera
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
 
