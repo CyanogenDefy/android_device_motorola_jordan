54b3f1abd461fd514c51be90a284f0b17e7578b7
Danny Baumann <dannybaumann@web.de>
2011-11-03 14:50:53 +0100

    Revert "do not prelink camera wrapper"
    This reverts commit 6d8d02954c21a296f6ebf23b33a2c119c2c06493.
    
    If prelinking is unwanted temporarily for debugging, the change should
    also be temporary.

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -9,7 +9,5 @@ LOCAL_SHARED_LIBRARIES:= libdl libutils libcutils libcamera_client
 LOCAL_MODULE := libcamera
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
 
