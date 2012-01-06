efb9311ec941f9db7e2ae2edf28128e10d9e706a
David Morgan <dmorgan81@gmail.com>
2012-01-06 12:04:21 -0500

    jordan: Depend on charge_only_mode from motorola_common
    Since other devices can use charge_only_mode, remove it
    from the jordan base and depend on charge_only_mode in
    motorola_common.
    
    Depends on http://r.cyanogenmod.com/11521
    
    Change-Id: Ica6c7335d96568e498d147313487d5eba2d0efb9

--- jordan.mk
+++ jordan.mk
@@ -95,7 +95,8 @@ PRODUCT_PACKAGES += \
 	DefyParts \
 	Usb \
 	su \
-	mot_boot_mode
+	mot_boot_mode \
+	charge_only_mode
 
 # for jpeg hw encoder/decoder
 # PRODUCT_PACKAGES += libskiahw libOMX.TI.JPEG.Encoder libOMX.TI.JPEG.decoder
